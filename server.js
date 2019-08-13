dd3b2ed7cf7b50e5e4f5f873b0ba0213ab4555b9
mario_nikolov <mnikolov@rnd-solutions.net>
2019-08-13 08:22:51 +0300

    add spa and static folder in server.js;

--- server.js
+++ server.js
@@ -14,6 +14,15 @@ if (process.env.NODE_ENV === 'development') {
 // Profile route
 app.use('/api/v1/profile', require('./routes/profile'))
 
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static(__dirname + '/public/'))
+
+    // handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+}
+
 const port = process.env.PORT || 8000
 app.listen(port, () => {
     console.log(`Server running in ${process.env.NODE_ENV} on port ${port}`)
